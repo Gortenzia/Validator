516c37a66f50aac48237c3c62b0429ed3c66d164
Gortenzia <tacumineka@gmail.com>
2022-02-17 17:17:03 +0300

    лишние папки

---
+++ src/js/__tests__/validator.test.js
@@ -0,0 +1,13 @@
+import Validator from '../validator';
+
+test.each([
+  ['Maria', true],
+  ['Zamzam326', false],
+  ['-Volvo432-', false],
+  ['_Solnshko_', false],
+  ['Kuba1986', false],
+  ['LIGA5678N', false],
+  ['Line668ln', true],
+])(('Проверка метода validateUsername'), (obj, exp) => {
+  expect(Validator.validateUsername(obj)).toBe(exp);
+});
